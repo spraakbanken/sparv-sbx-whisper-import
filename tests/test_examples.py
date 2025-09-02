4b84889c85763294aaa6319e911ba8ffc61c663d
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-09-02 15:02:09 +0200

    refactor: rename example to mp3

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,8 +5,8 @@ import sparv
 from syrupy.assertion import SnapshotAssertion
 
 
-def test_example_hello_whisper(setup_sparv: None, snapshot: SnapshotAssertion) -> None:  # noqa: ARG001
-    base_dir = Path("examples/hello-whisper")
+def test_example_hello_whisper_mp3(setup_sparv: None, snapshot: SnapshotAssertion) -> None:  # noqa: ARG001
+    base_dir = Path("examples/hello-whisper-mp3")
     export_dir = base_dir / "export"
     snakemake_dir = base_dir / ".snakemake"
     sparv_workdir = base_dir / "sparv-workdir"
@@ -17,7 +17,7 @@ def test_example_hello_whisper(setup_sparv: None, snapshot: SnapshotAssertion) -
     if sparv_workdir.exists():
         shutil.rmtree(sparv_workdir, ignore_errors=False)
 
-    args = ["--dir", "examples/hello-whisper", "run", "xml_export:pretty", "--log", "--cores", "1"]
+    args = ["--dir", "examples/hello-whisper-mp3", "run", "xml_export:pretty", "--log", "--cores", "1"]
 
     with sparv.call(args) as sparv_call:
         for log_message in sparv_call:
